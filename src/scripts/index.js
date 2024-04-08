195ffc335000ee643b180c944b7afdba2ce58314
Andrew Green <andrew@scepti.cc>
2024-04-08 01:29:46 -0400

    Add a dense black sphere to the middle

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,18 +11,25 @@ const element = document.getElementById('canvas'),
   engine = new Engine(canvas, physics);
 
 const light = new PointLight(0xff44aa, 500, 5000);
-  light.position.set(0, 0, 0);
+  light.position.set(0, 0, 0);  
 
 canvas.add(light);
 
-for(let i = 0; i < 1024; i++) {
+engine.addSphere(new SphericalBody({
+  mass: 2,
+  radius: 0.5,
+  position: new Vector3(0,0,0),
+  color: 0x000000
+}));
+
+for(let i = 0; i < 768; i++) {
   const d = Math.random() * 48 + 4,
     r = Math.random() * 2 * Math.PI,
     t = Math.random() * 2 * Math.PI,
-    size = Math.random() * 1.5;
+    size = Math.random() + 0.25;
 
   const sphere = new SphericalBody({
-    mass: size / 10,
+    mass: size / 50,
     radius: size,
     position: new Vector3(d * Math.sin(r) * Math.cos(t), d * Math.sin(r) * Math.sin(t), d * Math.cos(r)),
     color: 0xffffff      
@@ -32,47 +39,3 @@ for(let i = 0; i < 1024; i++) {
 }
 
 engine.start();
-
-/*import * as THREE from 'three';
-import Canvas from "./canvas";
-import { Engine, SphericalBody, Vector } from './physics';
-
-function makeLight() {
-  const color = 0x220088;
-  const intensity = 4000;
-  const light = new THREE.PointLight(color, intensity);
-  light.position.set(0, 0, 0);
-
-  return light;
-}
-
-const element = document.getElementById('canvas'),
-  canvas = new Canvas(element),
-  engine = new Engine();
-
-for(let i = 0; i < 128; i++) {
-  const d = Math.random() * 32 + 4,
-    r = Math.random() * 2 * Math.PI,
-    t = Math.random() * 2 * Math.PI,
-    size = Math.random();
-
-  const planet = new SphericalBody(size / 1000, size / 2);
-  planet.position = new Vector(d * Math.sin(r) * Math.cos(t), d * Math.sin(r) * Math.sin(t), d * Math.cos(r));
-  planet.velocity = planet.position.cross(new Vector(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)).unit().scale(Math.pow(1.064/d, 0.5));
-
-  engine.add(planet);
-  canvas.add(planet.mesh);
-  canvas.add(planet.light);
-}
-
-const core = new SphericalBody(1, 2, 0x000000);
-core.freeze = true;
-
-engine.add(core);
-canvas.add(core.mesh);
-
-//canvas.add(makeLight());
-
-engine.startSimulation(1000/30);
-canvas.startAnimation(false);
-setTimeout(() => engine.stopSimulation(), 1000);*/
